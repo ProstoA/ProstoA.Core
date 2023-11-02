1d56e7db512139d0a400935883aada80ff970554
Albert.Ibragimov <a@prosto-a.ru>
2023-11-02 13:35:42 +0300

    add localized concept

--- ProstoA.Core.Operations/OperationError.cs
+++ ProstoA.Core.Operations/OperationError.cs
@@ -1,7 +1,65 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 using Microsoft.Extensions.Logging;
 
 namespace ProstoA.Core.Operations;
+
+public interface ILocalizationProvider
+{
+    Task<Localized<T>> Get<T>(string key, Func<Localized<T>> getDefault, params string[] locales);
+}
+
+public class DictionaryLocalizationProvider : ILocalizationProvider
+{
+    private readonly Func<(string[] Keys, string[] Locales), Task<(string Key, (string Locale, object Value)[] Items)[]>> _reader;
+    private readonly ConcurrentDictionary<string, Task<(string Key, (string Locale, object Value)[] Items)[]>> _dictionary = new();
+    private readonly ConcurrentDictionary<string, Dictionary<string, string[]>> _requested = new();
+
+    public DictionaryLocalizationProvider(
+        Func<(string[] Keys, string[] Locales), Task<(string Key, (string Locale, object Value)[])[]>> reader)
+    {
+        _reader = reader;
+    }
+
+    public async Task<Localized<T>> Get<T>(string key, Func<Localized<T>> getDefault, params string[] locales)
+    {
+        var results = await _dictionary.GetOrAdd(key, _reader((new[] { key }, locales)));
+        var variants = results.Select(x => x.Items.ToDictionary(
+            xx => xx.Locale,
+            xx => xx.Value
+        )).FirstOrDefault() ?? new Dictionary<string, object>(0);
+        
+        return TryGet<T>(variants, locales, out var value) ? value! : getDefault();
+    }
+
+    private static bool TryGet<T>(
+        IDictionary<string, object> dictionary,
+        IEnumerable<string> locales,
+        out Localized<T>? result)
+    {
+        foreach (var locale in locales)
+        {
+            if (dictionary.TryGetValue(locale, out var value))
+            {
+                result = new Localized<T>((T)value, locale);
+                return true;
+            }
+        }
+
+        result = default;
+        return false;
+    }
+}
+
+public class Localized<TValue>
+{
+    public Localized(TValue value, string? locale = default)
+    {
+        locale ??= string.Empty; // Thread.CurrentThread.CurrentCulture.Name
+    }
+
+    public static implicit operator Localized<TValue>(TValue value) => new(value);
+}
 
 public record struct ErrorCode(string Value);
 
