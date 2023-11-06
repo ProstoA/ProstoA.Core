98262be26d7ba2c927c7e643f7cb25e00f28a3e8
Albert <a@prosto-a.ru>
2023-11-06 12:52:04 +0300

    add data concept

---
+++ ProstoA.Core.Data/Providers/DataSet.cs
@@ -0,0 +1,10 @@
+namespace ProstoA.Core.Data.Providers;
+
+/// <summary>
+/// Атомарная сущность храннеия данных в источнике (Таблица)
+/// </summary>
+public class DataSet
+{
+    // схема (column + constraints)
+    // строки
+}
---
+++ tests/ProstoA.Core.Data.Tests/Migrations/20231106020238_user_and_post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProstoA.Core.Data.Tests.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_and_post : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
